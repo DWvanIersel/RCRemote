67082565746451c8200d55de6eeca4c47260d70e
Diederik van Iersel <diederik.van.iersel@gmail.com>
2011-11-09 20:43:04 +0100

    force landscape orientation

--- src/com/dwvaniersel/rcremote/MainActivity.java
+++ src/com/dwvaniersel/rcremote/MainActivity.java
@@ -1,16 +1,19 @@
 package com.dwvaniersel.rcremote;
 
 import android.app.Activity;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 
 public class MainActivity extends Activity {
-    
 	
 	
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        
-    }
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE); //force landscape orientation
+		setContentView(R.layout.main);
+		
+		
+	}
 }
