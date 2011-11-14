173417893c4b7c3fb58365a2a1ffd6e0df2d3f19
Diederik van Iersel <diederik.van.iersel@gmail.com>
2011-11-14 04:27:01 -0800

    Made findDevice() method

--- src/com/dwvaniersel/rcremote/MainActivity.java
+++ src/com/dwvaniersel/rcremote/MainActivity.java
@@ -1,12 +1,19 @@
 package com.dwvaniersel.rcremote;
 
+import java.util.Iterator;
+import java.util.Set;
+
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends Activity {
 	
-	
+	private final String TAG = "MainActivity";
+	private final String DEVICENAME = "PFWSNXT";
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -14,6 +21,26 @@ public class MainActivity extends Activity {
 		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE); //force landscape orientation
 		setContentView(R.layout.main);
 		
+		
+	}
+	
+	public void findDevice() {
+		try {
+			BluetoothAdapter btInterface = BluetoothAdapter.getDefaultAdapter();
+			Set<BluetoothDevice> pairedDevices = btInterface.getBondedDevices();
+			Iterator<BluetoothDevice> it = pairedDevices.iterator();
+			while (it.hasNext()) {
+				BluetoothDevice bd = it.next();
+				if (bd.getName().equalsIgnoreCase(DEVICENAME)) {
+					connectToDevice(bd);
+				}
+			}
+		} catch (Exception e) {
+			Log.e(TAG, "Failed in findDevice() " + e.getMessage());
+		}
+	}
+	
+	public void connectToDevice(BluetoothDevice bd) {
 		
 	}
 }
